7562101b7c702fadd153c9f98cdbf0e6490ef88d
Daryl Vickerman <usansasa@gmail.com>
2018-10-21 16:44:10 +0200

    Added Code and GUI
    Minor Bugs that need to be fixed

---
+++ CrypticEditor/src/crypticeditor/CrypticEditor.java
@@ -0,0 +1,18 @@
+package crypticeditor;
+
+/**
+ *
+ * @author Crypto
+ */
+public class CrypticEditor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        EditorGUI GUI = new EditorGUI();
+        GUI.setVisible(true);
+        GUI.setTitle("Cryptic Editor");
+    }
+    
+}
